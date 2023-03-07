3dee098686262600100db8c1ce53cf0cc96b283b
Purvis Bedenbaugh <pbedenbaugh@hotmail.com>
2023-03-07 15:47:26 -0500

    got the basic simulation going

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,22 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    ###res[100:150, 100:150] = 1
+    ###res[300:, 200:] = 1
+    # if in center lower, back up
+    res[220:480,220:420] = -2
+    #if on left lower, go forward
+    res[220:479,0:219]=1
+    #if on left upper, go forward
+
+    res[0:219,0:219]=1
+    res[0:219,220:319]=0.25
+    # if far left, go forward fast
+    res[0:480,0:100]=2
+    # if far right, reverse
+    res[0:480,540:639]=-2
+    #if blank, creep forward
+    res[:,:]=res[:,:]=res[:,:]+0.1
     # ---
     return res
 
@@ -17,6 +31,21 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    #res[100:150, 100:300] = -1
+     # if in center lower, back up
+    res[220:480,220:420] = -2
+    #if on right lower, go forward
+    res[220:479,421:639]=1
+    #if on right upper, go forward
+    res[0:219,421:639]=1
+    res[0:219,320:420]=0.25
+    # if far right, go forward fast
+    res[0:480,540:639]=2
+
+    # if far left, go reverse
+    res[0:480,0:100]=-2
+    #if blank, creep forward
+    res[:,:]=res[:,:]=res[:,:]+0.1
+
     # ---
     return res
--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([171, 140, 100])
-upper_hsv = np.array([179, 200, 255])
-
+#lower_hsv = np.array([171, 140, 100])
+#upper_hsv = np.array([179, 200, 255])
+lower_hsv = np.array([2, 0, 80])
+upper_hsv = np.array([60, 255, 255])
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
     """Returns a 2D array"""
